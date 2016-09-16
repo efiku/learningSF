61b6d08151ea55c4608c1ffa7f54d69b925c4818
Krzysztof Pazdur <pazdurk@gmail.com>
2016-09-16 10:49:30 +0200

    Move configuration to yml

---
+++ src/efikuBundle/Controller/DefaultController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace efiku\Controller;
+
+use efiku\Services\DirectoryTweak;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Kernel;
+
+class DefaultController implements ContainerAwareInterface
+{
+    use ContainerAwareTrait;
+
+    public function indexAction()
+    {
+        $lottoNumbers = range(1, 9);
+        array_filter($lottoNumbers, function (&$number) {
+            $number = random_int(1, 99);
+            return $number;
+        });
+
+        $vendorSize = $this->container->get("directory.tool")->getDirectorySize(__DIR__ . "/../../../vendor/");
+
+        return $this->container->get("templating")->renderResponse('index.html.twig', [
+            'version' => Kernel::VERSION,
+            'lotto' => join(" ", $lottoNumbers),
+            'vendor_size' => $vendorSize
+        ], new Response());
+    }
+
+}
---
+++ src/efikuBundle/Services/DirectoryTweak.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: efik
+ * Date: 16.09.16
+ * Time: 10:09
+ */
+
+namespace efiku\Services;
+
+
+use FilesystemIterator;
+use RecursiveDirectoryIterator;
+use RecursiveIteratorIterator;
+use Symfony\Component\Filesystem\Exception\FileNotFoundException;
+
+class DirectoryTweak
+{
+
+    public function getDirectorySize($directory = "")
+    {
+        $bytesTotal = 0;
+        $path = realpath($directory);
+        if ($path === false) {
+            throw new FileNotFoundException("Directory \"$directory\" not exists!");
+        }
+        foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path,
+            FilesystemIterator::SKIP_DOTS)) as $object) {
+            $bytesTotal += $object->getSize();
+        }
+        return ceil($bytesTotal / 1024);
+
+    }
+
+}
