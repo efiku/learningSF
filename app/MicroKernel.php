f4f76a7d66aacee5d69c518edc313a1143bb753f
Krzysztof Pazdur <pazdurk@gmail.com>
2016-09-16 18:56:10 +0200

    Added Symfony Profiler

--- app/MicroKernel.php
+++ app/MicroKernel.php
@@ -15,22 +15,57 @@ class MicroKernel extends Kernel
 
     public function registerBundles()
     {
-        return
-            [
-                new FrameworkBundle(),
-                new TwigBundle(),
-                new efikuBundle()
-            ];
+        $bundles = [
+            new FrameworkBundle(),
+            new TwigBundle(),
+            new efikuBundle(),
+        ];
+
+        if ($this->isDevelopmentMode() ) {
+            $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
+        }
+
+        return $bundles;
     }
 
     protected function configureRoutes(RouteCollectionBuilder $routes)
     {
+        if($this->isDevelopmentMode()){
+            $routes->mount('/_wdt', $routes->import('@WebProfilerBundle/Resources/config/routing/wdt.xml'));
+            $routes->mount('/_profiler', $routes->import('@WebProfilerBundle/Resources/config/routing/profiler.xml'));
+        }
+
         $routes->import(__DIR__ . "/config/routing.yml");
     }
 
     protected function configureContainer(ContainerBuilder $c, LoaderInterface $loader)
     {
-        $loader->load(__DIR__ . '/config/framework.yml');
+        if($this->isDevelopmentMode()){
+            $c->loadFromExtension('web_profiler', ['toolbar' => true]);
+        }
 
+        $loader->load(__DIR__ . '/config/framework.yml');
+    }
+
+    public function getCacheDir()
+    {
+        if ($this->isDevelopmentMode()) {
+            return '/dev/shm/symfony/cache/' .  $this->environment;
+        }
+
+        return parent::getCacheDir();
+    }
+
+    public function getLogDir()
+    {
+        if ($this->isDevelopmentMode()) {
+            return '/dev/shm/symfony/logs';
+        }
+
+        return parent::getLogDir();
+    }
+
+    private function isDevelopmentMode(){
+        return in_array($this->getEnvironment(), array('dev', 'test'), true);
     }
 }
--- web/app.php
+++ web/app.php
@@ -1,6 +1,6 @@
 <?php
 use Symfony\Component\HttpFoundation\Request;
-require __DIR__.'/../vendor/autoload.php';
+require "/home/vagrant/symfony/vendor/autoload.php";
 require __DIR__.'/../app/MicroKernel.php';
 $kernel = new MicroKernel('dev', true);
 $request = Request::createFromGlobals();
