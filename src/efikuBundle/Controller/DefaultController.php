9fcc138f8a0b87fdb95d675a6575307915b5d28c
Krzysztof Pazdur <pazdurk@gmail.com>
2016-09-16 19:06:18 +0200

    Some changes in files

--- src/efikuBundle/Controller/DefaultController.php
+++ src/efikuBundle/Controller/DefaultController.php
@@ -14,12 +14,7 @@ class DefaultController implements ContainerAwareInterface
     public function indexAction()
     {
         $lottoNumbers = range(1, 9);
-        array_filter($lottoNumbers, function (&$number) {
-            $number = random_int(1, 99);
-            return $number;
-        });
-
-        $vendorSize = $this->container->get("directory.tool")->getDirectorySize(__DIR__ . "/../../../vendor/");
+        $vendorSize = $this->container->get("directory.tool")->getDirectorySize("/home/vagrant/symfony/vendor");
 
         return $this->container->get("templating")->renderResponse('index.html.twig', [
             'version' => Kernel::VERSION,
